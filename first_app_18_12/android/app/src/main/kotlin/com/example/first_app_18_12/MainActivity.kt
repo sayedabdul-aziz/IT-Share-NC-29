53819b961716fb2cbeec180000dc2e641dbd1680
“sayedabdul-aziz” <sayedabdulaziz10@gmail.com>
2023-12-18 21:04:26 +0200

    first commit

---
+++ first_app_18_12/android/app/src/main/kotlin/com/example/first_app_18_12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_app_18_12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ first_app_18_12/lib/first_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        backgroundColor: Colors.green,
+        title: const Text('My First App'),
+        centerTitle: true,
+        actions: const [
+          Icon(Icons.notifications),
+          Icon(Icons.search),
+        ],
+      ),
+    );
+  }
+}
